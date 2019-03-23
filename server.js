c56b35fafd68bdeea10151c892048118812ea765
Ball Man <joshua.christensen13@gmail.com>
2019-03-23 14:19:59 -0600

    beginning hw stuff

--- server.js
+++ server.js
@@ -1,41 +1,120 @@
 var express = require("express");
-var expressHandlebars = require("express-handlebars");
-var bodyParser = require("body-parser");
+var logger = require("morgan");
 var mongoose = require("mongoose");
+const cheerio = require('cheerio');
+
+const  request = require('request-promise');
+
+
+// Our scraping tools
+// Axios is a promised-based http library, similar to jQuery's Ajax method
+// It works on the client and on the server
+
+var db = require("./models");
 
 var PORT = process.env.PORT || 3000;
 
+// Initialize Express
 var app = express();
 
-var router = express.Router();
+// app.use(routes);
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Make public a static folder
+app.use(express.static("public"));
+// Configure middleware
+// Set Handlebars.
+var exphbs = require("express-handlebars");
 
-app.use(express.static(__dirname = "/public"));
-
-app.engine("handlebars", expressHandlebars({
-    defaultLayout: "main"
-}));
-
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
+// Import routes and give the server access to them.
+// var routes = require("./controller/routes");
 
-app.use(router);
+// Connect to the Mongo DB
+mongoose.connect("mongodb://localhost/Scrape_News", { useNewUrlParser: true });
 
-var db = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+app.get("/", function (req, res) {
+    console.log("is this working")
+    db.Article.find({}, function (error, data) {
+        var hbsObject = {
+            article: data
+        };
+        console.log(hbsObject);
+        res.render("home", hbsObject)
+    })
+})
 
-// mongoose.connect(MONGODB_URI);
+app.get("/saved", function (req, res) {
+    db.Article.find({ "saved": true }).populate("notes").exec(function (error, articles) {
+        var hbsObject = {
+            article: articles
+        };
+        res.render("saved", hbsObject);
+    })
+})
 
-mongoose.connect(db, function(error){
-    if (error) {
-        console.log(error);
-    }
-    else {
-        console.log("mongoose connection is successful")
-    }
+
+app.get("/scrape", function (req, res) {
+
+    (async () => {
+
+        await request('https://www.ksl.com/')
+            .then((res) => {
+
+                let $ = cheerio.load(res);
+                let records = [];
+
+                $("div[class='queue_story']").each((i, item) => {
+                    let title = $(item).find("div").find("h2").text();
+                    let summary = $(item).find("div").find("h5").text();
+                    let URL = $(item).find("div").find("a").attr("href");
+
+                    var record = {
+                        title,
+                        summary,
+                        URL: URL
+                    }
+                    records.push(record)
+                })
+                console.log('test', records);
+
+                if (records) {
+                    for(var i = 0; i < records.length; i++){
+                        let record = records[i];
+
+                        db.Article.create({
+                            title: record.title,
+                            summary: record.summary,
+                            URL: record.URL
+                        },
+                            function (err, inserted) {
+                                if (err) {
+                                    console.log(err);
+                                }
+                                else {
+                                    console.log(inserted)
+                                }
+                            },
+                        )
+                    }
+
+                }
+    })
+        res.send("Scrape Complete");
+  
+  }) ()
 });
 
-app.listen(PORT, function(){
-    console.log("Listening on port:" + PORT);
+app.get("/articles", function (req, res) {
+
 })
+
+
+app.listen(PORT, function () {
+    console.log("App running on port " + PORT + "!");
+});
